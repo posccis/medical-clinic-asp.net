f8760ed3ecc3aea5866efddf57813fe3db6b3a20
Posccis <lkzq33@gmail.com>
2021-12-19 23:54:14 -0300

    feat(main): Added the search by Id and the Create in Medico

--- medical-clinic-rest-api/medical-clinic-rest-api/Controllers/MedicoController.cs
+++ medical-clinic-rest-api/medical-clinic-rest-api/Controllers/MedicoController.cs
@@ -19,7 +19,7 @@ namespace medical_clinic_rest_api.Controllers
         {
             _dbContext = dbContext;
         }
-
+        ///List all the doctors
         [HttpGet]
         public async Task<IActionResult> GetAllMedicos() 
         {
@@ -39,6 +39,37 @@ namespace medical_clinic_rest_api.Controllers
 
             return Ok(medicos);
         }
+        ///Search for a doctor with the code
+        [HttpGet("[action]")]
+        public async Task<IActionResult> MedicoByCod(int Codigo) 
+        {
+            var medico = await _dbContext.Medicos.Where(a => a.CodMed == Codigo).ToListAsync();
+            return Ok(medico);
+        }
+
+        ///Create a doctor
+        [HttpPost]
+        public async Task<IActionResult> MedicoCreate([FromBody] Medico medico)
+        {
+            await _dbContext.Medicos.AddAsync(medico);
+            await _dbContext.SaveChangesAsync();
+            return StatusCode(StatusCodes.Status201Created);
+        }
+
+        //[HttpPut]
+        //public async Task<IActionResult> MedicoUpdate([FromBody]string column, string valor, int codigo) 
+        //{
+
+        //    _dbContext.Medicos.FromSqlRaw("UPDATE medico set " + column.ToString() + "=" + valor.ToString() + " where CodMed=" + codigo);
+        //    await _dbContext.SaveChangesAsync();
+
+        //    return StatusCode(StatusCodes.Status200OK);
+
+
+            
+        //}
+
+
 
 
     }
